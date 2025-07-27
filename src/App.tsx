9e58278d8f9512fd299b9c2f1c1d1ba21f143c92
Derek Song <dereksong28@gmail.com>
2025-07-27 15:36:32 -0400

    started work on brand page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ComponentViewer from './pages/ComponentViewer'
 import AssetViewer from './pages/Assets'
+import Brand from './pages/Brand'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
       <Route path="/assets" element={<AssetViewer />} />
-        <Route path="/component/:id" element={<ComponentViewer />} />
+      <Route path="/component/:id" element={<ComponentViewer />} />
+      <Route path="/brand" element={<Brand />} />
       </Routes>
     </BrowserRouter>
   )
