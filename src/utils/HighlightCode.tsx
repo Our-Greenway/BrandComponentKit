bbe697cf03d27f694230db104edbba26e66d2c48
Derek Song <dereksong28@gmail.com>
2025-08-28 15:59:04 -0400

    Update HighlightCode.tsx

--- src/utils/HighlightCode.tsx
+++ src/utils/HighlightCode.tsx
@@ -26,19 +26,6 @@ function highlightCode(code: string, lang: string): string {
     return `<pre class="text-white font-mono whitespace-pre">${highlighted}</pre>`;
   }
 
-  if (lang === 'js') {
-    const escaped = code.replace(/</g, '&lt;').replace(/>/g, '&gt;');
-    const highlighted = escaped
-      .replace(/(\/\/.*$|\/\*[\s\S]*?\*\/)/gm, `<span class="text-gray-500">$1</span>`) // comments - added multiline flag
-      .replace(/("(?:[^"\\]|\\.)*")|('(?:[^'\\]|\\.)*')|(`(?:[^`\\]|\\.)*`)/g, `<span class="text-green-400">$&</span>`) // strings - fixed non-capturing groups
-      .replace(/\b\d+(?:\.\d+)?\b/g, `<span class="text-yellow-300">$&</span>`) // numbers - fixed non-capturing group
-      .replace(/\b(?:const|let|var|if|else|for|while|do|function|return|import|from|export|default|class|extends|super|new|try|catch|finally|throw|switch|case|break|continue|typeof|instanceof|in|of|await|async|static|get|set)\b/g, `<span class="text-purple-400">$&</span>`) // keywords - used non-capturing group
-      .replace(/\b([a-zA-Z_][\w]*)\s*(?=\()/g, `<span class="text-blue-400">$1</span>`) // function names
-      .replace(/(\+|-|\*|\/|%|=|==|===|!=|!==|<|>|<=|>=|&&|\|\||!|\?|:|\.\.\.)/g, `<span class="text-pink-400">$&</span>`); // operators
-  
-    return `<pre class="text-white font-mono whitespace-pre">${highlighted}</pre>`;
-  }
-
   return `<pre class="text-white font-mono whitespace-pre">${code}</pre>`;
 }
 
